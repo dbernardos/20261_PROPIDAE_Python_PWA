d084ca4c55229878284af23042cb4765ed9398d5
dbernardos <121075844+dbernardos@users.noreply.github.com>
2026-04-08 14:22:34 -0300

    correção views

--- core/views.py
+++ core/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .form import UsuarioForm
+
 
 def index(request):
     return render(request, 'index.html')
