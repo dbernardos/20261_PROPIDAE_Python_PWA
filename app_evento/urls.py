3f5f0228835c8283e88a8c064684720b2242d032
dbernardos <121075844+dbernardos@users.noreply.github.com>
2026-08-11 18:56:23 -0300

    Alteração pasta detalhes do evento

--- app_evento/urls.py
+++ app_evento/urls.py
@@ -9,6 +9,10 @@ app_name = 'evento'
 
 urlpatterns = [
     path('cadastrar_evento/', views.cadastrar_evento, name='cadastrar_evento'),
+    path('editar_evento/<int:evento_id>/', views.cadastrar_evento, name='editar_evento'),
+    path('excluir_evento/<int:evento_id>/', views.excluir_evento, name='excluir_evento'),
+    path('eventos/<int:evento_id>/', views.detalhes_evento, name='detalhes_evento'),
+
     path('cadastrar_atividade/<int:evento_id>/', views.cadastrar_atividade, name='cadastrar_atividade'),
 
     path('dados/', views.dados, name='dados'),
